e79981b83959d69aab25be5ba1626b35360ede0f
herocodecamp <herocodecamp@gmail.com>
2023-03-18 20:10:17 +0600

    update

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
 
 // database connection 
 const dbConnection = () => {
   mongoose
     .connect(
-      `${process.env.DB_URL}`
+      `${process.env.DB_URL}`,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
     )
     .then((data) => console.log("Connection successful"))
     .catch((error) => console.log(error));
