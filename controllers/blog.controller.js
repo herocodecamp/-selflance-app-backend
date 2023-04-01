15be80a88b103d26495287a148314d64f35fcd7b
jasim.herocodecamp <jasim.herocodecamp@gmail.com>
2023-04-01 19:15:56 +0600

    add put api for blog data

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -2,7 +2,6 @@ const Blogs = require("../models/Blogs");
 const Users = require("../models/User");
 
 exports.blogController = async (req, res) => {
-
   try {
     const newBlog = new Blogs(req.body);
     await newBlog.save();
@@ -14,7 +13,6 @@ exports.blogController = async (req, res) => {
 };
 
 exports.findBlogController = async (req, res) => {
-
   try {
     const result = await Blogs.find({});
     res.status(200).json(result);
@@ -24,10 +22,20 @@ exports.findBlogController = async (req, res) => {
 };
 
 exports.deleteBlogController = async (req, res) => {
-  
   try {
     const deleteBlog = await Blogs.findByIdAndDelete(req.params.id);
     res.status(200).json({ message: "successfully delete a blog" });
+  } catch (error) {
+    res.status(500).json({ massage: error.massage, type: error.name });
+  }
+};
+
+exports.updateBlogController = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await Blogs.findByIdAndUpdate(id, { $set: req.body });
+    res.status(200).json({ message: "successfully updated blog" });
   } catch (error) {
     res.status(500).json({ massage: error.massage, type: error.name });
   }
--- routes/blog.routes.js
+++ routes/blog.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { blogController, findBlogController, deleteBlogController } = require("../controllers/blog.controller");
+const { blogController, findBlogController, deleteBlogController, updateBlogController } = require("../controllers/blog.controller");
 const Router = express.Router();
 
 Router.route('/:id')
 .post(blogController)
 .delete(deleteBlogController)
+.put(updateBlogController)
 
 Router.get('/', findBlogController)
 
