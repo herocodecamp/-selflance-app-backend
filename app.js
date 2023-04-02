744c1135c5e1123be0693555f34386fd97ef375d
jasim.herocodecamp <jasim.herocodecamp@gmail.com>
2023-04-02 14:58:35 +0600

    agrement schema added

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 const appRouter = require("./routes");
-const cors = require('cors');
-
-app.use(cors())
-app.use(express.json())
 
 // //api version control
 app.use("/api/v1", appRouter);
