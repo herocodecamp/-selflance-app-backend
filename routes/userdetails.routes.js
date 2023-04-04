05a986a4e75fe36db728ba72544ac73de75fcf65
hasnat-herocodecamo <hasnat.herocodecamp@gmail.com>
2023-04-04 17:41:35 +0500

    multer storage middleware

--- routes/userdetails.routes.js
+++ routes/userdetails.routes.js
@@ -1,30 +1,6 @@
 const express = require('express');
 const {createUserDetails,readUserDetails,updateUserDetails,deleteUserDetails} = require('../controllers/userdetail.controller');
-const multer = require("multer");
-const path = require("path");
-
-
-// multer storage
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-   
-      cb(null, "images");
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
-      }
-    // filename: function (req, file, cb) {
-    //   cb(
-    //     null,
-    //     file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    //   );
-    // },
-  });
-
-
-
-const upload = multer({ storage: storage });
+const upload = require('../middleware/multerStorage')
 
 
 
